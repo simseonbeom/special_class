593cebadeb098c28ed9eaed329901f0c2a59c4c0
kindtiger <seonbeom2@gmail.com>
2023-07-08 15:49:16 +0900

    1주차 특강
    - getCss
    - setCss

--- advanced/client/js/1-1.js
+++ advanced/client/js/1-1.js
@@ -53,18 +53,81 @@ function getCss(node,prop){
 
   return getComputedStyle(node)[prop];
 }
+function setCss(node,prop,value){
+  // const {node,prop,fontSize,msg,active} = options;
+  if(typeof node === 'string'){
+    node = $(node);
+  }
+  
+  if(!(prop in document.body.style)){
+    syntaxError('setCss 함수의 두 번째 인자인 prop은 유효한 css 속성이 아닙니다.')
+  }
 
+  if(!value){
+    syntaxError('setCss 함수의 세 번째 인자인 value는 필수 입력값 입니다.')
+  }
+  
+  node.style[prop] = value;
+  
+}
 
 const result = getCss('h1','font-size');
 
 
 
 // setCss(node:string, prop:string, value:string) : void
+// console.log( result );
 
-console.log( result );
+const user = {
+  nickName:'tiger',
+  age:28,
+  kind:true
+}
+
+// for(let key in user){
+//   console.log(user[key]);
+// }
+
+
+
+// setCss({
+//   node:'h1',
+//   prop:'font-size',
+//   value:'50px',
+//   msg:'에러지롱',
+//   active:true
+// });
 
 
 // getCss(node: string, prop: string):string
+
+
+
+
+// module program
+// 변수의 유효범위 
+
+// (function(css){
+
+//   console.log( css );
+
+
+// })(css)
+
+
+
+
+// function css(node,prop,value){
+//   return !value ? getCss(node,prop) : setCss(node,prop,value);
+// }
+
+
+
+
+
+
+
+
 
 // node의 값을 문자로 받을 경우
 
